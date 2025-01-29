269d85fc95a0f21959547bb62ec98b79e2baaf29
Manthan Srivastava <manthan.learner@gmail.com>
2025-01-29 13:33:00 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ def main(context):
         context.error("Could not list users: " + repr(err))
 
     # 1) Parse JSON body for lat, lng, searchTerm, and targetGbp
-    context.log("Raw body: " + context.req.body)
+    context.log(type(context.req.body))
     try:
         body = json.loads(context.req.body)
     except:
